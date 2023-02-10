6f71d6cce4e8ab95206544395ea3d25686fbd1bd
Sergazina Diana <124108363+naddb@users.noreply.github.com>
2023-02-10 11:31:32 +0600

    Add files via upload

---
+++ lab1/variables.py
@@ -0,0 +1,95 @@
+x=12
+y='hiii'
+print(x)
+print(y)
+
+x=12
+x=13
+print(x)
+
+x=12
+x='p'
+print (x)
+
+x = str(3)   
+y = int(3)    
+z = float(3)
+print (x, y, z)  
+
+print (type (x))
+
+A=1
+a='vfgbedhjnk'
+print (a)
+print (A)
+
+'''
+3prin = 4
+r-r = 5
+r r = 5
+print (3prin, r-r, r r)''' #case sensitive 
+
+#camel case
+myVariableName = 1 
+#pascal case 
+MyVariableName = 1
+#snake case
+my_variable_name = 1
+
+a,b,c = 'Paulo', 'Raylie', 'Danny'
+print (a)
+print (b)
+print (c)
+print (a, b, c)
+
+a = b = c = 'Paulo'
+print (a, b, c)
+
+a = ['1', '2', '3']
+x, y, z = a 
+print (x, y, z)
+print (a)
+
+print (x + y + z)
+#print(xyz)
+
+y = float(2)
+x = 1
+print (x + y)
+
+x = 'Diana'
+
+def myfunc():
+  print('my name is', x) #('my name is ' + x)
+
+myfunc()
+
+x = 'Diana'
+
+def myfunc():
+  x = 'Dina'
+  print('my name is ' + x)
+
+myfunc()
+
+print('my name is ' + x)
+
+def myfunc():
+    global x 
+    x = 'Diana'
+
+myfunc()
+
+print('my name is ' + x)
+
+x = 'Dina'
+
+def myfunc():
+    global x 
+    x = 'Diana'
+
+myfunc()
+
+print('my name is ' + x)
+
+carname = 'Volvo'
