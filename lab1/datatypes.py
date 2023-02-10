398068883f397454fd256770f8884431343a5f67
Sergazina Diana <124108363+naddb@users.noreply.github.com>
2023-02-10 11:33:14 +0600

    Add files via upload

---
+++ lab1/datatypes.py
@@ -0,0 +1,26 @@
+x = 1
+print(type(x))
+
+x = 'aaaaa' #str	
+x = 1 #int	
+x = 1.2	#float	
+x = 1j #complex	
+print(x)
+x = ["apple", "banana", "cherry"] #list
+x = ("apple", "banana", "cherry") #tuple	
+x = range(6) #range
+print(x)
+x = {"name" : "John", "age" : 36} #dict	
+print(x)
+x = {"apple", "banana", "cherry"} #set
+x = frozenset({"apple", "banana", "cherry"}) #frozenset	
+print(x)	
+x = True #bool	
+x = b"Hello" #bytes
+print(x)		
+x = bytearray(5) #bytearray
+print(x)		
+x = memoryview(bytes(5)) #memoryview
+print(x)		
+x = None #NoneType	
+print(x)	
