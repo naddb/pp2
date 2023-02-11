140e39439801efc2c73effe680d0d578a6de4f9b
Sergazina Diana <124108363+naddb@users.noreply.github.com>
2023-02-11 15:42:21 +0600

    Delete lab3/functions1 directory

--- lab3/functions1/2.py
+++
@@ -1,6 +0,0 @@
-
-def convert(far:int):
-    c = (5 / 9) * (far-32)
-    print(c)
-convert(1000)
-convert(123454)
--- lab3/functions1/6.py
+++
@@ -1,7 +0,0 @@
-#Write a function that accepts string from user, return a sentence with the words reversed. 
-#We are ready -> ready are We
-def reversed_sentence(sentence):
-    sentence1=sentence.split(' ')
-    sentence1.reverse()
-    print(*sentence1)
-reversed_sentence("We Are Ready")
